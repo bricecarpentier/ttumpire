ecd37c3aa7c4912bab617ae35c07101097c4010a
Brice Carpentier <brice@daknet.org>
2020-10-31 11:07:29 +0100

    integrate with devtools

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useReduxDevToolsExtension } from '@react-navigation/devtools';
 
 import styles from './App.styles';
 import Home from './scenes/home';
@@ -12,11 +16,14 @@ declare const global: { HermesInternal: null | {} };
 const Stack = createStackNavigator();
 
 const App = () => {
+  const navigationRef = React.useRef<NavigationContainerRef>(null);
+  useReduxDevToolsExtension(navigationRef);
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.safeArea}>
-        <NavigationContainer>
+        <NavigationContainer ref={navigationRef}>
           <Stack.Navigator headerMode="none">
             <Stack.Screen name="home" component={Home} />
             <Stack.Screen name="ingame" component={InGame} />
