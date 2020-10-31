003f1b0f4f4b0f91fee5561ddfa548b7225220b8
Brice Carpentier <brice@daknet.org>
2020-10-31 20:00:14 +0100

    rename HomeScreenButton into NavigationScreenButton

--- scenes/home/Home.component.tsx
+++ scenes/home/Home.component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
-import HomeScreenButton from '../../components/HomeScreenButton';
+import NavigationScreenButton from '../../components/NavigationScreenButton';
 
 import styles from './Home.styles';
 import { EventHandler } from '../types/eventhandler';
@@ -12,7 +12,7 @@ interface HomeProps {
 const Home = (props: HomeProps) => {
   return (
     <ScrollView style={styles.root} centerContent>
-      <HomeScreenButton
+      <NavigationScreenButton
         title="NEW GAME"
         onPress={props.newGame}
         backgroundStyle={styles.buttonBackground}
